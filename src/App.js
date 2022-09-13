ed6d5763cc5ea942d76a3cf5cd7c89b0f58f5c85
amit-changepond <111910309+amit-changepond@users.noreply.github.com>
2022-09-14 01:32:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Created By Amit
         </a>
       </header>
     </div>
